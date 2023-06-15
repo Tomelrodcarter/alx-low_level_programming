14febcadef419f43aa79b4e7b33d6103f8577c96
tomelrodcarter <tomose374@gmail.com>
2023-06-15 16:40:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that put a line with put fn
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+Return(0);
+}
